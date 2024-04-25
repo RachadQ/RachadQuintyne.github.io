45ddf10a50ec920c23a2ce660e96f897b9e1541f
Rachad <36934217+RachadQ@users.noreply.github.com>
2024-04-24 22:11:29 -0400

    Update the gitifnore file and configuration

--- my-ts-react-app/src/firebase_setup/firebaseConfig.ts
+++ my-ts-react-app/src/firebase_setup/firebaseConfig.ts
@@ -1,12 +1,12 @@
 // firebaseConfig.ts
 const firebaseConfig = {
-    apiKey: "AIzaSyCO5bsj1RB0gsNxLvRzX4rjhGPGaIynBoA",
-    authDomain: "profile-aa69c.firebaseapp.com",
-    projectId: "profile-aa69c",
-    storageBucket: "profile-aa69c.appspot.com",
-    messagingSenderId: "644369874226",
-    appId: "1:644369874226:web:2b5f8bad9233569796ab2b",
-    measurementId: "G-NND3GW9NKZ"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   };
   
   export default firebaseConfig;
